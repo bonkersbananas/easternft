0b4ac66d7693a7f30e93c794ea8e845a20f11fba
Carl Oscar Aaro <hello@carloscar.com>
2022-04-02 16:03:02 +0200

    whitespace

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,11 @@
 async function main() {
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
- 
+
     // Start deployment, returning a promise that resolves to a contract object
-    const hello_world = await HelloWorld.deploy("Hello World!");   
+    const hello_world = await HelloWorld.deploy("Hello World!");
     console.log("Contract deployed to address:", hello_world.address);
  }
- 
+
  main()
     .then(() => process.exit(0))
     .catch(error => {

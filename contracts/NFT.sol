c3852e471043c72bff7f709e27f3b1f64dd8e262
Carl Oscar Aaro <hello@carloscar.com>
2022-04-14 22:05:54 +0200

    Updated owners

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -13,15 +13,16 @@ contract NFT is ERC721A {
     using Strings for uint256;
 
     mapping (address => bool) private _contractOwners;
-    string private _tokenBaseURI = 'https://bafybeidy5lzy2des2s2debhvnhqttv3v7i5owjd4vlknaylay7y4ar5rai.ipfs.infura-ipfs.io/';
+    string private _tokenBaseURI = 'https://easter.infura-ipfs.io/ipfs/QmNU1eLytFQLp1QL4WSdR9wNHmCFjF1RdmksvXx6E3T6S2/';
     bool public isMintable = true;
     uint256 public maxSupply = 400;
-    uint256 private _price = 1.00 ether;
+    uint256 private _price = 1.00 ether; /* 1.00 MATIC */
 
     constructor() ERC721A("EasterNFT", "EASTERNFT") {
         _contractOwners[0x39bEb60bc4c1b8b0eBeEDC515c7A56e7DfB3a5A9] = true;
         _contractOwners[0x9eaD888876b3978E8b138d4B6416111255B89e03] = true;
         _contractOwners[0xAeB37Ff5C6a7f43e721849873EdA2dAe2b6871F9] = true;
+        _contractOwners[0x77366B0B45f6c67F181bdfDF1C19e2FA2733db73] = true;
     }
 
     function mint() external payable {

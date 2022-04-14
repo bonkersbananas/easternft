8a40332b1e9d5cdcd8e970c0f3a6d1cbc6069fcf
Carl Oscar Aaro <hello@carloscar.com>
2022-04-14 22:56:13 +0200

    Added gasPrice to scripts

--- scripts/close_mint.js
+++ scripts/close_mint.js
@@ -1,8 +1,15 @@
 const { contractInterface } = require("./contract/interface");
 
 async function main() {
+    let options = {};
+    if (process.env.DEPLOY_NETWORK !== 'localhost') {
+        let provider = new ethers.providers.AlchemyProvider(network=deployNetwork);
+        const feeData = await provider.getFeeData();
+        options = { gasPrice: feeData.gasPrice };
+    }
+
     await contractInterface.isMintable();
-    await contractInterface.toggleMintable(false);
+    await contractInterface.toggleMintable(false, options);
     await contractInterface.isMintable();
 }
 
--- scripts/open_mint.js
+++ scripts/open_mint.js
@@ -1,8 +1,15 @@
 const { contractInterface } = require("./contract/interface");
 
 async function main() {
+    let options = {};
+    if (process.env.DEPLOY_NETWORK !== 'localhost') {
+        let provider = new ethers.providers.AlchemyProvider(network=deployNetwork);
+        const feeData = await provider.getFeeData();
+        options = { gasPrice: feeData.gasPrice };
+    }
+
     await contractInterface.isMintable();
-    await contractInterface.toggleMintable(true);
+    await contractInterface.toggleMintable(true, options);
     await contractInterface.isMintable();
 }
 
